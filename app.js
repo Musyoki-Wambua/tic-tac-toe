2fee4437e6ad8a9c499c1a9f3ace7990405ee48e
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-08-03 09:23:31 +0300

    Done

--- app.js
+++ app.js
@@ -31,10 +31,31 @@ function addGo (event) {
 function checkScore () {
 
     const allSquares = document.getElementsByClassName("square")
-    console.log(allSquares)
+    // console.log(allSquares)
     const winningCombos = [
         [0,1,2], [3,4,5], [6,7,8],
         [0,3,6], [1,4,7], [2,5,8], 
         [2,4,6], [0,4,8]
     ]
+
+    winningCombos.forEach(array => {
+       const circleWins =  array.every(cell => allSquares[cell].firstChild?.classList.contains('circle'))
+
+       if (circleWins) {
+        infoDisplay.textContent = "Circle Wins!"
+        allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+        return
+       }
+    })
+
+    winningCombos.forEach(array => {
+        const crossWins =  array.every(cell => allSquares[cell].firstChild?.classList.contains('cross'))
+ 
+        if (crossWins) {
+         infoDisplay.textContent = "Cross Wins!"
+         allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+         return
+        }
+     })
+
 }
